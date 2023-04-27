9a6108f989b4bd0f19fc8b00c92bd2846f43a637
Cesar Puac <jmangochango642@gmail.com>
2023-04-27 11:42:59 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# CV website
+## Mis habilidades para crear sitios web han estado mejorando, por lo que comparto mi CV website personal, donde hablo más sobre mí, mis proyectos y cómo contactarme.
+
+![Captura de pantalla de mi cv websit](./img/captura-cv-website.png)
---
+++ scripts/index.js
@@ -0,0 +1,50 @@
+const API = "https://api.github.com/users/Jahn13"
+const containerProjects = document.querySelector("#containerCards")
+
+const repoData = (data) =>  {
+    let guardarCards = "";
+
+    data.forEach(element => {
+    let cards = `
+    <div class="card mb-3 col-12 col-md-6 col-lg-4">
+        <div class="row g-0">
+            <div class="col-md-4">
+                <img src="${element.owner.avatar_url}" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">${element.name}</h5>
+                    <p class="card-text">${element.description}</p>
+                    <a href="${element.html_url}" target="__Blank" class="btn btn-primary">Ir al repositorio</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    `
+    guardarCards += cards
+    });
+    containerProjects.innerHTML = guardarCards;
+}
+
+const fetchData = async (url) => {
+    try{
+        let options =  { "headers": 
+                            {
+                                "Authorization": "token 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"
+                            }
+                        }
+        let response = await fetch(url, options);
+        let data = await response.json();
+        let repos = await fetch(data.repos_url, options)
+        let responseRepos = await repos.json();
+
+        console.log(responseRepos)
+        repoData(responseRepos);
+    }catch(error){
+        console.log(error)
+    }
+}
+
+fetchData(API);
+
+
